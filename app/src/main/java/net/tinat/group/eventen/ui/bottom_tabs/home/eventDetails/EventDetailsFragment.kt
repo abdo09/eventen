7011b1007e8ed4b9ef46642a7617df4630049c43
abdo94 <abdo.wr@hotmail.com>
2022-01-12 10:10:03 +0200

    Implement ViewBinding

--- app/src/main/java/net/tinat/group/eventen/ui/bottom_tabs/home/eventDetails/EventDetailsFragment.kt
+++ app/src/main/java/net/tinat/group/eventen/ui/bottom_tabs/home/eventDetails/EventDetailsFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.event_details_fragment.view.*
+import kotlinx.android.synthetic.main.toolbar.view.*
 import net.tinat.group.eventen.R
 import net.tinat.group.eventen.base.BaseSupportFragment
 import net.tinat.group.eventen.databinding.EventDetailsFragmentBinding
@@ -28,6 +30,6 @@ class EventDetailsFragment : BaseSupportFragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        binding.appBarLayout.toolbar_layout.tv_title.text = resources.getString(R.string.besudan_event)
     }
 }
